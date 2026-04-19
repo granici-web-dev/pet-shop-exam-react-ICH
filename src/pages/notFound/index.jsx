ad3a2a61ed6d110d22da75f306ba4dfcac1ff059
Serghei_Granici <granici.design@gmail.com>
2026-04-19 16:24:05 +0200

    Created 404 page with adaptive

--- src/pages/notFound/index.jsx
+++ src/pages/notFound/index.jsx
@@ -1,7 +1,30 @@
 import styles from './styles.module.css';
+import NotFoundImage from '../../assets/images/404.png';
+import Headline from '../../components/headline';
+import { useNavigate } from 'react-router-dom';
+import Button from '../../components/ui/button';
 
 function NotFound() {
-  return <div>404</div>;
+  const navigate = useNavigate();
+
+   const setNavigate = (path) => {
+     navigate(`/${path}`);
+   };
+
+  return (
+    <section className={styles.notFound}>
+      <div className={styles.container}>
+        <img className={styles.notFoundImage} src={NotFoundImage} alt="404" />
+        <div className={styles.contentDiv}>
+          <Headline title={'Page Not Found'} />
+          <p className={styles.notFoundText}>
+            We’re sorry, the page you requested could not be found. Please go back to the homepage.
+          </p>
+          <Button onClick={() => setNavigate('/')} title={'Go Home'} />
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default NotFound;
